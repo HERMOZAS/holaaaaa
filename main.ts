91013578ce5710ef2aef9542545fee4795f06c72
HERMOZAS <130647395+HERMOZAS@users.noreply.github.com>
2023-04-17 08:26:31 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,11 @@
+basic.forever(function () {
+    basic.showLeds(`
+        . # . # .
+        # # # # #
+        # # # # #
+        . # # # .
+        . . # . .
+        `)
+    basic.pause(90)
+    basic.showIcon(IconNames.SmallHeart)
+})
